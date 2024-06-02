a196df49fca5921083ff440c24dc48dea90821f1
Dan-Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-06-02 22:30:09 +0100

    Work on allauth staticfiles
    exporter allauth static files into project dir so I can change the html and css composition

--- tinytalefactory/settings.py
+++ tinytalefactory/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third Party Apps
     'allauth',
     'allauth.account',
 
@@ -99,7 +100,11 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-STATIC_URL = 'static/'
+#
+STATIC_URL = 'staticfiles/'
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles/']
+#
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
