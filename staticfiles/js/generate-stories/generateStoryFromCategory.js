d2214e130a2639b1a2dfea2fc771b93b4ad629d3
Dan-Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-08-21 00:10:57 +0100

    Profile page functionality finished

---
+++ staticfiles/js/generate-stories/generateStoryFromCategory.js
@@ -0,0 +1,41 @@
+function showDropdown (element) {
+    const nameAttribute = element.textContent.trim();
+    const dropDown = document.getElementById(nameAttribute);
+    const generateButton = document.getElementById(nameAttribute + '-button');
+
+    generateButton.setAttribute('data-protect', 'yes');
+
+    dropDown.style.display === 'none' ? dropDown.style.display = 'block' : siblingsDisplayNone(element);
+    generateButton.style.display === 'none' ? generateButton.style.display = 'block' : generateButton.style.display = 'none';
+}
+
+function siblingsDisplayNone(element) {
+
+    Array.from(element.parentNode.children).slice(1).forEach(child => {
+        if (!child.hasAttribute('data-protect')) {
+            child.style.display = 'none';
+        }
+    });
+}
+
+function continueWithStory (element) {
+    const formId = element.id.replace('button', 'form');
+    const formElement = document.getElementById(formId);
+
+    formElement.style.display === 'none' ? formElement.style.display = 'block' : formElement.style.display = 'none';
+}
+
+function generateFromCategory (element) {
+
+    catName = element.name.toLowerCase().split(' ').join('_');
+
+    const urlQuery = `?category=${catName}`;
+    const url = baseGenerateUrl + urlQuery;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log('Json Response: ', data))
+        .catch(error => console.log('Error:', error));
+
+    // TODO: When getting the response, check if valid, and if valid get link to show story.
+}
--- staticfiles/js/profile/profileScript.js
+++ staticfiles/js/profile/profileScript.js
@@ -2,15 +2,19 @@ const userInfoListItems = document.querySelectorAll('.user-info-list-items');
 const onHoverWrapperElement = document.querySelectorAll('.on-hover-wrapper')[0];
 const profileWrapperRight = document.getElementById('profile-wrapper-right');
 const changeUserInfoElement = document.getElementById('change-user-info');
+const showStoriesElement = document.getElementById('display-stories');
 
 const emailSettingsEl = document.getElementById('email-settings');
 const setPasswordEl = document.getElementById('password');
 
-
+// TODO: When having the baseViewStoryURL add it onto here.. so the button works
+const baseViewStoryUrl = '#';
 
 emailSettingsEl.addEventListener('click', createIframeElement);
 setPasswordEl.addEventListener('click', createIframeElement);
 setPasswordEl.addEventListener('click', createIframeElement);
+
+showStoriesElement.addEventListener('click', showStories);
 
 // Display help text on icon hover
 Array.from(userInfoListItems)
@@ -97,14 +101,13 @@ function createIframeElement(event) {
 // Change email iFrame TODO: Maybe a different approach
 
 function addStylesheetToPasswordFrameDocument() {
-    console.log('PASSWORD SET');
     const frameElement = document.getElementById('set-password-frame');
 
     const contentDocument = frameElement.contentDocument;
     const activeBodyElement = contentDocument.activeElement;
 
     const availableButtons = activeBodyElement.querySelectorAll('form button');
-    console.log(availableButtons);
+
     availableButtons.forEach(button => {
         button.addEventListener('click', () => {
             frameElement.style.opacity = '0';
@@ -126,7 +129,7 @@ function addStylesheetToPasswordFrameDocument() {
 
 
 function addStylesheetToEmailFrameDocument() {
-    console.log('entered!')
+
     const frameElement = document.getElementById('change-email-frame');
 
     const contentDocument = frameElement.contentDocument;
@@ -162,8 +165,11 @@ function displayChangeUserInfoDiv(event) {
 
     // create a new wrapper
     const wrapperElement = document.createElement('div');
+    wrapperElement.style.tranition = 'opacity 1s linear';
+    wrapperElement.style.opacity = '0';
     wrapperElement.classList.add('right-info-wrapper');
     profileWrapperRight.append(wrapperElement);
+    setTimeout(() => wrapperElement.style.opacity = '100', 10);
 
     // create the header
     const headerElement = document.createElement('h2');
@@ -289,7 +295,7 @@ function submitNewProfileInfo(event) {
             const successElement = createSuccessElement();
             addElementToWrapper(successElement);
         })
-        .catch(error => console.log('Error with edit data: ', error));
+        .catch(error => console.error('Error with edit data: ', error));
 
     function createSuccessElement() {
 
@@ -309,9 +315,6 @@ function submitNewProfileInfo(event) {
             setTimeout(() => {rightInfoWrapperElement.removeChild(element)}, 1)
         }, 10000)
     }
-
-
-
 }
 
 function clearProfileWrapperRight(){
@@ -323,3 +326,106 @@ function clearChildrenSelectedClass(node) {
         child.classList.remove('selected');
     });
 }
+
+// Show stories button functionality
+async function showStories(event) {
+    try {
+        clearChildrenSelectedClass(event.target.parentNode);
+        event.target.classList.add('selected');
+        const stories = await getStories();
+        const storyElements = createStoryCards(stories);
+        displayStoryElements(storyElements);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getStories() {
+
+    try {
+        const response = await fetch(getStoriesURL);
+        const responseJson = await response.json();
+        const responseValues = Array.from(Object.values(responseJson));
+        return responseValues;
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+function createStoryCards(storiesArray) {
+    const generatedElements = new Array();
+    const fontAwesomeBookClass = 'fa-solid fa-book-open-reader';
+
+    storiesArray.forEach(story => {
+        const storySlug = story['slug'];
+        const storyTitle = story['title'];
+        const storyImage = story['info']['urls'][0];
+        const createdAt = story['created_at'].split('T')[0].replaceAll('-', '/');
+
+        // Card Element Creation
+        const storyCardEl = document.createElement('div');
+        storyCardEl.classList.add('story-card');
+
+        // Left - Media side
+        const storyMediaEl = document.createElement('div');
+        storyMediaEl.classList.add('story-media');
+        storyCardEl.append(storyMediaEl);
+
+        const imgEl = document.createElement('img');
+        imgEl.setAttribute('src', storyImage);
+        storyMediaEl.append(imgEl);
+
+        // Right - Article side
+        const articleEl = document.createElement('article');
+        articleEl.classList.add('story-info-article');
+        storyCardEl.append(articleEl);
+
+        const titleEl = document.createElement('h3');
+        titleEl.textContent = storyTitle;
+        articleEl.append(titleEl);
+
+        const createdOnEl = document.createElement('p');
+        createdOnEl.textContent = 'Created On: ';
+        articleEl.append(createdOnEl);
+
+        const dateEl = document.createElement('date');
+        dateEl.textContent = createdAt;
+        createdOnEl.append(dateEl);
+
+        const linkEl = document.createElement('a');
+        linkEl.textContent = 'View story';
+        linkEl.setAttribute('href', baseViewStoryUrl + storySlug);
+        articleEl.append(linkEl);
+
+        const bookEl = document.createElement('i');
+        bookEl.className = fontAwesomeBookClass;
+        articleEl.append(bookEl);
+
+        generatedElements.push(storyCardEl);
+    });
+
+    return generatedElements;
+}
+
+function displayStoryElements(storiesArray) {
+    clearProfileWrapperRight();
+
+    const storiesWrapperEl = document.createElement('div');
+    storiesWrapperEl.classList.add('stories-wrapper');
+    storiesWrapperEl.style.opacity = '0';
+
+    const headerEl = document.createElement('h2');
+    headerEl.textContent = 'All your generated stories';
+    storiesWrapperEl.append(headerEl)
+
+    storiesArray.forEach(storyDiv => storiesWrapperEl.append(storyDiv));
+
+    profileWrapperRight.append(storiesWrapperEl);
+
+    setTimeout(() => storiesWrapperEl.style.opacity = '100', 10);
+}
+
+// On first load of page
+
+setTimeout(() => showStories(showStoriesElement.click()), 1000);
