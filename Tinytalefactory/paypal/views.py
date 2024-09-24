c9e1cee1d7dd1f11ae182238e69e5d6f47b39007
Dan-Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-09-24 09:35:50 +0100

    Fixed paypal notifications and displaying wrong message on successful payment

--- Tinytalefactory/paypal/views.py
+++ Tinytalefactory/paypal/views.py
@@ -1,35 +1,16 @@
-from django.shortcuts import render
-from django.urls import reverse
 from django.views import generic as views
 
-import requests
-
-from Tinytalefactory.common.helpers import create_tokens_purchased_notification
 from Tinytalefactory.paypal.models import Order
 
 
 class SuccessView(views.TemplateView):
     template_name = 'paypal/success.html'
 
-    def get(self, request, *args, **kwargs):
-        order_id = self.request.GET.get('token', '')
-        if self._validate_order(order_id):
-            order = Order.objects.filter(order_id=order_id).first()
-            create_tokens_purchased_notification(request.user, order.quantity, order.order_id, order.price)
-        return super().get(request, *args, **kwargs)
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         order_id = self.request.GET.get('token', '')
         context['order_id'] = order_id
-        context['valid_order'] = self._validate_order(order_id)
         return context
-
-    @staticmethod
-    def _validate_order(order_id):
-        return (Order.objects.filter(order_id=order_id).exists()
-                and
-                Order.objects.filter(order_id=order_id).first().status == 'completed')
 
 
 class CancelView(views.TemplateView):
--- staticfiles/js/paypal/paymentExecute.js
+++ staticfiles/js/paypal/paymentExecute.js
@@ -1,3 +1,6 @@
+const articleEl = document.getElementById('article-element')
+profileUrl = '/profile/';
+
 async function paymentExecute (){
     try {
         const options = {
@@ -11,6 +14,14 @@ async function paymentExecute (){
 
         const response = await fetch(paymentExecuteUrl, options);
 
+        if (response.status === 200) {
+            createSuccessMessage();
+            return;
+        }
+
+        createErrorMessage();
+        return;
+
     } catch (error) {
         console.error(error);
     }
@@ -18,4 +29,28 @@ async function paymentExecute (){
 
 if (orderId) {
     paymentExecute();
+}
+
+function createSuccessMessage() {
+    articleEl.innerHTML = `<h2>Thank you for your purchase</h2>
+                <p>
+                    Your order with order id:
+                    <br>
+                    <strong class="bold">${orderId}</strong>
+                    <br>
+                    has been successful.
+                    <br>
+                    It might take up a minute for your account to be credited the bough tokens.
+                </p>
+                <p>
+                    You can now create more exciting stories using our platform.
+                    Go to your <a href=${profileUrl}>profile page</a> to view your total tokens.
+                </p>`
+}
+
+function createErrorMessage() {
+    articleEl.innerHTML = `<h2>Oh, no!</h2>
+                <p>
+                    There is nothing to see here. Go <a href="#" onclick="history.back()">back</a>.
+                </p>`
 }
