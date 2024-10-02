84d124fa08f920a587bdb116e496d56e4e4a1b6c
Dan-Mihaylov <123562461+Dan-Mihaylov@users.noreply.github.com>
2024-10-03 00:33:58 +0100

    Cleaned up permission mixin, added custom messages and 403.html file

--- Tinytalefactory/auth_mixins/mixins.py
+++ Tinytalefactory/auth_mixins/mixins.py
@@ -1,24 +1,29 @@
-from django.contrib.auth.mixins import AccessMixin
-from ..generate_stories.models import Story
-
 from allauth.account.models import EmailAddress
-from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
+from django.contrib.auth.mixins import AccessMixin
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist, PermissionDenied
+
+from ..generate_stories.models import Story
 
 
 # TODO: When the view is written, add the permission.
 class OwnerOfStoryRequiredMixin(AccessMixin):
-
-    permission_denied_message = 'You do not have permission for this resource'
+    permission_denied_message = 'This resource is not available to you.'
 
     def dispatch(self, request, *args, **kwargs):
-        slug = kwargs.get('slug', '')
-        has_story = request.user.stories.filter(slug=slug).exists()
-        story_is_public = self._check_story_is_public(slug)
+        try:
+            if not request.user.is_authenticated:
+                raise PermissionDenied(self.permission_denied_message)
 
-        if has_story or story_is_public:
-            return super().dispatch(request, *args, **kwargs)
+            slug = kwargs.get('slug', '')
+            has_story = request.user.stories.filter(slug=slug).exists()
+            story_is_public = self._check_story_is_public(slug)
 
-        return self.handle_no_permission()
+            if not has_story and not story_is_public:
+                raise PermissionDenied(self.permission_denied_message)
+        except PermissionDenied or Exception as e:
+            return self.handle_no_permission()
+
+        return super().dispatch(request, *args, **kwargs)
 
     @staticmethod
     def _check_story_is_public(slug):
@@ -31,18 +36,17 @@ class OwnerOfStoryRequiredMixin(AccessMixin):
 
 
 class CanGenerateStoryMixin(AccessMixin):
-
     permission_denied_message = ('You do not have permission for this resource, your email might not be verified, '
                                  'you might not have enough tokens to generate a story, or you might have already '
                                  'used your demo story generation.')
 
     def dispatch(self, request, *args, **kwargs):
-        user = request.user
-
-        if not user.is_authenticated or not user.email:
-            return self.handle_no_permission()
-
         try:
+            user = request.user
+
+            if not user.is_authenticated or not user.email:
+                raise PermissionDenied(self.permission_denied_message)
+
             email = user.email
             verified_email = self._check_email_verified(email)
             available_tokens = user.tokens.total_tokens()
@@ -50,15 +54,15 @@ class CanGenerateStoryMixin(AccessMixin):
             # TODO: When creating the db table to store if email has already used generation do validations here
 
             if not all([verified_email, available_tokens > 0]):
-                raise Exception
-            
+                raise PermissionDenied(self.permission_denied_message)
+
             return super().dispatch(request, *args, **kwargs)
 
         except MultipleObjectsReturned or ObjectDoesNotExist or AttributeError or Exception:
             return self.handle_no_permission()
 
-    @staticmethod
-    def _check_email_verified(email):
-        email_obj = EmailAddress.objects.get(email=email)
-        return email_obj.verified
 
+@staticmethod
+def _check_email_verified(email):
+    email_obj = EmailAddress.objects.get(email=email)
+    return email_obj.verified
